018cde9b437d987a7b15f08007e4afd11ec45941
amayro <aabmstu@gmail.com>
2018-08-15 00:01:29 +0300

    all change

--- hw_lesson03/hw03.sql
+++ hw_lesson03/hw03.sql
@@ -3,10 +3,12 @@
 ---------------------------------------------------------------------------
 use geodata;
 
+-- 1.
 SELECT _countries.title as country, _regions.title as region, _cities.title as city FROM _cities 
 	INNER JOIN _countries on _cities.country_id = _countries.id
     INNER JOIN _regions on _cities.region_id = _regions.id;
-
+	
+-- 2.
 SELECT _countries.title as country, _regions.title as region, _cities.title as city FROM _cities 
 	INNER JOIN _countries on _cities.country_id = _countries.id
     INNER JOIN _regions on _cities.region_id = _regions.id
@@ -15,42 +17,41 @@ SELECT _countries.title as country, _regions.title as region, _cities.title as c
 	
 	
 ---------------------------------------------------------------------------
--- employees --
+-- employees --  254466
 ---------------------------------------------------------------------------
 
 use employees;
 
-SELECT dept_name, AVG(salary) FROM dept_emp 
+-- 1. Выбрать среднюю зарплату по отделам.
+SELECT dept_name, AVG(salary) as avg_salary FROM dept_emp 
 	INNER JOIN salaries on dept_emp.emp_no = salaries.emp_no
     INNER JOIN departments on dept_emp.dept_no = departments.dept_no
     WHERE dept_emp.to_date = '9999-01-01' AND salaries.to_date = '9999-01-01'
     group by dept_name;
 
-
-SELECT CONCAT(last_name, ' ', first_name) AS full_name, max(salary) FROM salaries 
+-- 2. Выбрать максимальную зарплату у сотрудника.
+SELECT salaries.emp_no, CONCAT(last_name, ' ', first_name) AS full_name, max(salary) as max_salary FROM salaries 
 	INNER JOIN employees on salaries.emp_no = employees.emp_no
-    group by full_name;
+    WHERE salaries.to_date = '9999-01-01'
+    group by salaries.emp_no;
     
     
-    
-SELECT CONCAT(last_name, ' ', first_name) AS full_name, max(salary) as max_salary FROM dept_emp 
-	INNER JOIN salaries on dept_emp.emp_no = salaries.emp_no
-    INNER JOIN departments on dept_emp.dept_no = departments.dept_no
-    INNER JOIN employees on dept_emp.emp_no = employees.emp_no
-    WHERE dept_emp.to_date = '9999-01-01' AND salaries.to_date = '9999-01-01'; 
+-- 3. Удалить одного сотрудника, у которого максимальная зарплата.
+DELETE FROM employees 
+	WHERE employees.emp_no = (SELECT salaries.emp_no FROM salaries 
+    WHERE salaries.to_date = '9999-01-01' and salary = (select max(salary) FROM salaries));
     
     
-    
-    
-    
-    
-SELECT count(dept_emp.dept_no) AS count_emp FROM dept_emp 
+-- 4. Посчитать количество сотрудников во всех отделах.
+SELECT count(dept_emp.emp_no) AS count_emp FROM dept_emp 
 	INNER JOIN salaries on dept_emp.emp_no = salaries.emp_no
     INNER JOIN departments on dept_emp.dept_no = departments.dept_no
     WHERE dept_emp.to_date = '9999-01-01' AND salaries.to_date = '9999-01-01';    
     
+    
+-- 5. Найти количество сотрудников в отделах и посмотреть, сколько всего денег получает отдел.
 SELECT dept_name, count(dept_emp.dept_no) AS count_emp, sum(salary) AS sum_salary FROM dept_emp 
 	INNER JOIN salaries on dept_emp.emp_no = salaries.emp_no
     INNER JOIN departments on dept_emp.dept_no = departments.dept_no
     WHERE dept_emp.to_date = '9999-01-01' AND salaries.to_date = '9999-01-01'
-    group by dept_name;    
+    group by dept_name;  
