e5c09290572639eaff231143100dcb1d4ff6efeb
amayro <aabmstu@gmail.com>
2018-08-29 14:18:17 +0300

    task 2

--- hw_lesson07/hw07.sql
+++ hw_lesson07/hw07.sql
@@ -16,3 +16,35 @@ DROP DATABASE IF EXISTS employees;
 
 mysql -uroot -p1234 employees < dump_employees.sql
 
+
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+
+
+DROP DATABASE IF EXISTS vk;
+CREATE DATABASE IF NOT EXISTS vk;
+
+CREATE TABLE user_ (
+    id          INT             NOT NULL    AUTO_INCREMENT,  
+    name        VARCHAR(30)     NOT NULL,
+	PRIMARY KEY (id)
+);
+	
+CREATE TABLE like_ (
+    id          INT             NOT NULL    AUTO_INCREMENT,  
+    from_id     INT             NOT NULL,
+    to_id       INT             NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (fk_from_id)  REFERENCES user_ (id),
+	FOREIGN KEY (fk_to_id)    REFERENCES user_ (id)
+);
+	
+	
+	
+SELECT user_.id, user_.name, count(like_.from_id) as get_like, count(like_.to_id) as count_liked
+	FROM like_
+	JOIN user_ on like_.from_id = user_.id
+	GROUP BY user_.id;
+	
+	
